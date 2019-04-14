7fe30f8e532b7ccca02b03efdc29b962b466a171
van-9 <753340374@qq.com>
2019-04-14 12:24:56 +0800

    login access

--- app/src/main/java/com/example/fzuscore/LoginActivity.java
+++ app/src/main/java/com/example/fzuscore/LoginActivity.java
@@ -1,56 +1,19 @@
 package com.example.fzuscore;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -84,48 +47,61 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mAccountNumber =  findViewById(R.id.et_account_number);
+        mAccountNumber = findViewById(R.id.et_account_number);
 
-        mPassword =  findViewById(R.id.et_password);
+        mPassword = findViewById(R.id.et_password);
         FloatingActionButton fab = findViewById(R.id.fab_sign_in);
-        fab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-
-                OkHttpClient client = new OkHttpClient.Builder()
-                        .connectTimeout(10, TimeUnit.SECONDS)
-                        .writeTimeout(10,TimeUnit.SECONDS)
-                        .readTimeout(20, TimeUnit.SECONDS)
-                        .build();
-                LoginAccess loginAccess = new LoginAccess("00000000", "00000000");
-                Gson gson = new Gson();
-                String json = gson.toJson(loginAccess);
-                String url= "http://47.112.10.160:3389/api/login";
-                RequestBody requestBody = FormBody.create(MediaType.parse("application/json; charset=utf-8")
-                        , json);
-                Request request = new Request.Builder()
-                        .url(url)
-                        .post(requestBody)
-                        .build();
-
-                //创建/Call
-                Call call = client.newCall(request);
-                //加入队列 异步操作
-                call.enqueue(new Callback() {
-                    //请求错误回调方法
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        System.out.println("连接失败");
-                    }
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        System.out.println(response.body().string());
-                    }
-                });
-//                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-//                startActivity(intent);
-            }
+        fab.setOnClickListener(v -> {
+            sendRequestWithOkHttp();
         });
+    }
+//        fab.setOnClickListener(new View.OnClickListener()) {
+//            @Override
+//            public void onClick (View view){
+//                sendRequestWithOkHttp();
+////                OkHttpClient client = new OkHttpClient.Builder()
+////                        .connectTimeout(10, TimeUnit.SECONDS)
+////                        .writeTimeout(10,TimeUnit.SECONDS)
+////                        .readTimeout(20, TimeUnit.SECONDS)
+////                        .build();
+////                LoginAccess loginAccess = new LoginAccess("00000000", "00000000");
+////                Gson gson = new Gson();
+////                String json = gson.toJson(loginAccess);
+////                String url= "http://47.112.10.160:3389/api/login";
+////                RequestBody requestBody = FormBody.create(MediaType.parse("application/json; charset=utf-8")
+////                        , json);
+////                Request request = new Request.Builder()
+////                        .url(url)
+////                        .post(requestBody)
+////                        .build();
+//            }
+
+
+    void sendRequestWithOkHttp() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    LoginAccess loginAccess = new LoginAccess("00000000", "00000000");
+                    Gson gson = new Gson();
+                    String json = gson.toJson(loginAccess);
+                    String url = "http://47.112.10.160:3389/api/login";
+                    RequestBody requestBody = FormBody.create(MediaType.parse("application/json; charset=utf-8")
+                            , json);
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .post(requestBody)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    System.out.println(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
     }
 
@@ -140,4 +116,5 @@ public class LoginActivity extends AppCompatActivity {
      * errors are presented and no actual login attempt is made.
      */
 }
+
 
